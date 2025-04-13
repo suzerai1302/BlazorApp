22272f9c5b323110c252fc15f27a4ffdb3580b26
suzerai1302 <ladoresgiovani@gmail.com>
2025-04-13 17:13:08 +0800

    fixed projects folder structure

--- Pages/Home.razor.cs
+++
@@ -1,9 +0,0 @@
-﻿using BethanysPieShopHRM.App.Components.Widgets;
-
-namespace BethanysPieShopHRM.App.Pages
-{
-    public partial class Home
-    {
-        public List<Type> Widgets { get; set; } = new List<Type>() { typeof(EmployeeCountWidget), typeof(InboxWidget) };
-    }
-}
